c8cfce901504eafcac408f226800561133756c81
Ramaditia D <rdwiyan.saputra@gmail.com>
2018-01-14 21:46:33 +0800

    added production config

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,43 @@
 const path = require('path');
+const webpack = require('webpack');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
-module.exports = {
-    entry: "./src/app.js",
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/, // convert all file '.js' to es5
-            exclude: '/node_modules'
-        }, {
-            test: /\.css$/,
-            use: [
-                'style-loader',
-                'css-loader'
-            ]
-        }]
-    },
+module.exports = (env) => {
+    const isProduction = env === 'production';
+    return Object.assign({
+        entry: "./src/app.js",
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/, // convert all file '.js' to es5
+                exclude: '/node_modules'
+            }, {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }]
+        }
+    }, (isProduction ? production : development));
+};
+
+const development = {
     devtool: 'cheap-module-eval-source-map',
     devServer: {
         contentBase: path.join(__dirname, 'public')
     }
-};
+}
+
+const production = {
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify('production')
+        }),
+        new UglifyJsPlugin()
+    ]
+}
